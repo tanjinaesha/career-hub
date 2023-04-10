29a5fd01e6764b540376f0e90ef91929726c5e36
tanjinaesha <eshaislam15@gmail.com>
2023-04-10 23:50:56 +0600

    folder added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes,Route,BrowserRouter} from 'react-router-dom';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './components/Home/Home';
+
+import JobApply from './components/JobApply/JobApply';
+import Jobs from './components/Jobs/Jobs ';
+import Navbar from './components/Navbar/Navbar';
+import NotFound from './components/NotFound/NotFound';
+import Statics from './components/Statics/Statics';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+
+    <BrowserRouter>
+<Routes>
+       
+       
+<Route  path="/" element={<Navbar></Navbar>} >
+              
+              </Route>
+        
+       
+          
+        <Route  path="/Home" element={<Home></Home>} >
+              
+              </Route>
+           
+            <Route path="/applied-jobs" element={<JobApply></JobApply>}>
+            
+            </Route>
+            <Route path="/statistics" element={ <Statics></Statics>}>
+             
+            </Route>
+            <Route path="*" element={  <NotFound></NotFound>}>
+            
+            </Route>
+          
+        
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
+
 
 export default App;
