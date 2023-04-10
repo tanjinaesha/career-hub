809402cc9e9eed87defe234a451409060e92632a
tanjinaesha <eshaislam15@gmail.com>
2023-04-11 01:59:44 +0600

    new

---
+++ solution/src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import CategorySection from '../CategorySection/CategorySection';
+
+import Jobs from '../Jobs/Jobs ';
+
+
+const Home = () => {
+  return (
+    <div>
+    
+        <Banner></Banner>
+      <CategorySection></CategorySection>
+      <Jobs></Jobs>
+    
+    </div>
+  );
+};
+
+export default Home;
---
+++ solution/src/components/Statics/Statics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statics = () => {
+  return (
+    <div>
+      this is statics
+    </div>
+  );
+};
+
+export default Statics;
